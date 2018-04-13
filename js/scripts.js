1028e11837ddab5ca070276969417ca4d2bc7f53
Ewa <grtxhu@gmail.com>
2018-04-13 23:38:35 +0200

    Change script.js and index.html

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+
+var a = prompt('podaj długość podstawy, a'),
+    h = prompt('podaj wysokość, h'),
+    triangleArea = a*h/2;
+
+alert('Pole trójkąta wynosi:' + triangleArea);
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
